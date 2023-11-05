22927aa0686adeb8d68a4403d541635b01c73395
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-05 12:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 function calculateMinCost() {
-  //your code here
-	let minCost=0;
-let ropeLengthList = document.querySelector("#rope-lengths").value;
-ropeLengthList = ropeLengthList.split(',').map(Number);
-	while(ropeLengthList.length>0)
-		{
-			ropeLengthList = ropeLengthList.sort((a,b)=>{return a-b;});
-			minCost = ropeLengthList[0]+ropeLengthList[1];
-			ropeLengthList = ropeLengthList.slice(2,ropeLengthList.length);
-			ropeLengthList.push(minCost);
-		}  
-	const resultDiv = document.getElementById('result');
-            resultDiv.innerText = minCost;
+    let ropeLengthList = document.querySelector("#rope-lengths").value;
+    ropeLengthList = ropeLengthList.split(',').map(Number);
+
+    if (ropeLengthList.length < 2) {
+        // Handle cases with less than two ropes
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerText = "Minimum Cost: 0";
+        return;
+    }
+
+    // Sort the array in ascending order
+    ropeLengthList = ropeLengthList.sort((a, b) => a - b);
+
+    let minCost = 0;
+
+    while (ropeLengthList.length > 1) {
+        // Get the two smallest rope lengths
+        const smallest1 = ropeLengthList.shift();
+        const smallest2 = ropeLengthList.shift();
+
+        // Calculate the cost and update the list
+        minCost += smallest1 + smallest2;
+        ropeLengthList.push(smallest1 + smallest2);
+        // Re-sort the array (you can use binary heap for optimization)
+        ropeLengthList = ropeLengthList.sort((a, b) => a - b);
+    }
+
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerText = "Minimum Cost: " + minCost;
 }
 
 
