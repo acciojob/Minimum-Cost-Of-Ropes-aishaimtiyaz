28c47442ee0ce1baca6806d9250762e85be05ab6
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-05 12:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
-}  
+	let minCost=0;
+let ropeLengthList = document.querySelector("#rope-lengths").value;
+ropeLengthList = ropeLengthList.split(',').map(Number);
+	while(ropeLengthList.length>0)
+		{
+			ropeLengthList = ropeLengthList.sort((a,b)=>{return a-b;});
+			minCost = ropeLengthList[0]+ropeLengthList[1];
+			ropeLengthList = ropeLengthList.slice(2,ropeLengthList.length);
+			ropeLengthList.push(minCost);
+		}  
+	const resultDiv = document.getElementById('result');
+            resultDiv.innerText = minCost;
+}
+
+
+
+
+
